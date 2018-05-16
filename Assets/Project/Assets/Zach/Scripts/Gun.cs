3075b0b377ab1ae97c71762abf4fd2c5c443ca5f
Zach Oriel <Zacharyaaron15@gmail.com>
2018-05-15 17:59:47 -0700

    Weapon firing, switching, and reload

---
+++ Assets/Project/Assets/Zach/Scripts/Gun.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+
+    public int maxAmmo = 10;
+    private int currentAmo;
+    public float reloadTime = 3f;
+    public bool isReloading = false;
+
+    public Camera fpsCam;
+    //public ParticleSystem muzzleFlash;
+    //public GameObject impactEffect;
+    //public AudioSource reloadSound;
+
+    public float nextTimeToFire = 0f;
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentAmo = maxAmmo;
+	}
+
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (isReloading)
+        {
+            return;
+        }
+
+        if (currentAmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+            if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire && currentAmo > 0)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+	}
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+
+        //reloadSound.Play();
+
+        Debug.Log("Reloading...");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - 0.25f);
+
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(0.25f);
+
+        currentAmo = maxAmmo;
+        isReloading = false;
+    }
+
+    void Shoot()
+    {
+        //muzzleFlash.Play();
+
+        currentAmo--;
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(hit.normal * -impactForce);
+            }
+
+            //GameObject impactObject = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            //Destroy(impactObject, 2f);
+        }
+    }
+}
