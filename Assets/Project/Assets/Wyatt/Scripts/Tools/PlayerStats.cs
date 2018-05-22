88b8f3d847f1ae45eaaeb9d0a01ffe7acb183ecc
Wyatt Gallagher <s179053@students.aie.edu.au>
2018-05-22 10:42:47 -0700

    better stuff for player

--- Assets/Project/Assets/Wyatt/Scripts/Tools/PlayerStats.cs
+++ Assets/Project/Assets/Wyatt/Scripts/Tools/PlayerStats.cs
@@ -4,7 +4,14 @@ using UnityEngine.SceneManagement;
 public class PlayerStats : MonoBehaviour
 {
     public EnemyGun gun;
+    public int Lives;
+    public float Score;
 
+    public GameObject spawnPoint;
+
+
+
+    public float armor;
     public float health;
     public float startHealth = 100f;
 
@@ -15,8 +22,15 @@ public class PlayerStats : MonoBehaviour
 
     public void TakeDamage(float amount)
     {
-        health -= amount;
+        if(armor > 0)
+        {
+             armor -= amount;
 
+        }
+        else if(armor <= 0)
+        {
+            health -= amount;
+        }
         if (health <= 0f)
         {
             Die();
@@ -25,6 +39,17 @@ public class PlayerStats : MonoBehaviour
 
     void Die()
     {
-        SceneManager.LoadScene("TestingScene");
+        health = startHealth;
+        armor = 0;
+        gameObject.transform.position = spawnPoint.transform.position;
+        Lives--;
+    }
+
+    void GameOver()
+    {
+        if(Lives <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
