47b786bf562cdad8267767965a624eeb74826944
Wyatt Gallagher <s179053@students.aie.edu.au>
2018-06-04 11:23:28 -0700

    this wouldnt have happened you know

--- Assets/Project/Assets/Wyatt/Scripts/Tools/PlayerStats.cs
+++ Assets/Project/Assets/Wyatt/Scripts/Tools/PlayerStats.cs
@@ -29,11 +29,7 @@ public class PlayerStats : MonoBehaviour
     {
         if(scoreText != null)
         {
-<<<<<<< HEAD
             scoreText = GameObject.Find("/UI/UICanvas/BasePanel/ScorePanel/scoreText").GetComponent<Text>();
-=======
-            scoreText = GameObject.Find("/UI/UICanvas/BasePanel/ScorePanel/scoreText").GetComponent<Text>(); 
->>>>>>> dev
         }
         if (livesText != null)
         {
